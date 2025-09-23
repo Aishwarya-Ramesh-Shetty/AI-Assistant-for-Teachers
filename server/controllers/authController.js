da0fa4170820b3560a09be647acf3279819c7f29
Aishu-bit <shettyaishu050@gmail.com>
2025-09-23 17:21:28 +0530

    Created Sign up and Login in backend

---
+++ server/controllers/authController.js
@@ -0,0 +1,59 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken'); 
+
+exports.register = async(req,res) =>{
+    try{
+        const {name,email,password,role} = req.body;
+
+        const existing = await User.findOne({email})
+        if(existing) return res.status(400).json({message:"User already exists"})
+
+        const hashed = await bycrypt.hash(password,10);
+
+        const user = await User.create({name,email,password:hashed,role})
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+        res.json({
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role
+            }
+        })
+        console.log("Successfully registered")
+    }
+    catch(err){
+        console.error("Registration error",err.message)
+        res.status(500).json({message:"Server error"})
+    }
+}
+
+exports.login = async(req,res) =>{
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email})
+        if(!user) return res.status(400).json({message:"Invalid email"})
+        
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch) return res.status(400).json({message:"Invalid password"})
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+
+        res.json({
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role
+            }
+        })
+        console.log("Successfully logged in")
+    }
+    catch(err){
+        console.error("Login error",err.message)
+        res.status(500).json({message:"Server error"})
+    }
+}
